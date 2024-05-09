7cdf020143ba3dbe037d9d95c5fc1da77cecf28b
solaiman-siam <solaimansiam93@gmail.com>
2024-05-10 03:49:54 +0600

    added footer section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,66 +62,6 @@ function Navbar() {
           <div className="lg:flex hidden items-center gap-4">
             {user?.email ? (
               <>
-                <img
-                  id="avatarButton"
-                  type="button"
-                  data-dropdown-toggle="userDropdown"
-                  data-dropdown-placement="bottom-start"
-                  className="w-10 h-10 rounded-full cursor-pointer"
-                  src="/docs/images/people/profile-picture-5.jpg"
-                  alt="User dropdown"
-                />
-
-                {/* <!-- Dropdown menu --> */}
-                <div
-                  id="userDropdown"
-                  className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
-                >
-                  <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-                    <div>Bonnie Green</div>
-                    <div className="font-medium truncate">
-                      name@flowbite.com
-                    </div>
-                  </div>
-                  <ul
-                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="avatarButton"
-                  >
-                    <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Dashboard
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Settings
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Earnings
-                      </a>
-                    </li>
-                  </ul>
-                  <div className="py-1">
-                    <a
-                      href="#"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Sign out
-                    </a>
-                  </div>
-                </div>
-
                 <button
                   onClick={handleSignOut}
                   type="button"
